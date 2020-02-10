fc3f4a7104fd582cd9c80741877282e936709d6b
ArtifaxXx <reutov.alexey@yahoo.com>
2020-02-10 22:26:14 +0000

    create exchange_data file and test it

--- tests/test_legacy_strategy_code.py
+++ tests/test_legacy_strategy_code.py
@@ -1,5 +1,13 @@
 from main.legacy_strategy_code import main
+from main.exchange_data import get_exchange_data
 
 
 def test_code_runs():
     main()
+
+
+def test_exchange_data():
+    result = get_exchange_data('binance', 'TRX/BNB', '1d')
+    assert len(result['candle_data']) == 500
+    assert result['ticker']
+    assert result['trades']
